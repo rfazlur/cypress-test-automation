5f61cbbd242d5a44add5829d0f789fec6ecbafd1
Faiz <fazlur.faiz.rahman@gmail.com>
2019-07-21 23:22:06 +0700

    add computer tc

--- cypress/integration/Navigation/navigation.spec.js
+++ cypress/integration/Navigation/navigation.spec.js
@@ -14,4 +14,17 @@ describe('computer database website navigation', () => {
         cy.get('header a').should('contain', 'Play sample application — Computer database');
     });
 
+    it('should be able to add new computer', () => {
+        cy.get('header a').should('contain', 'Play sample application — Computer database');
+        cy.get('#add').should('be.visible');
+        cy.get('#add').click();
+        cy.get('.btn.primary').should('be.visible');
+        cy.get('#name').type('New Computer 2');
+        cy.get('#introduced').type('2001-12-12');
+        cy.get('#discontinued').type('2018-12-12');
+        cy.get('#company').select('RCA');
+        cy.get('.btn.primary').click();
+        cy.get('.alert-message.warning').should('contain', 'Done! Computer New Computer 2 has been created');
+    });
+
 });
